e3fafcc880987c7f991ed194d89e4bd8ca4bcf03
emenegger <evanemenegger@gmail.com>
2026-01-05 12:00:45 -0600

    update map header

--- pages/photography/MapLayout.tsx
+++ pages/photography/MapLayout.tsx
@@ -32,32 +32,16 @@ const MapLayout = ({ images }: { images: Images[] }) => {
       className="relative min-h-screen bg-[#e5e7eb] p-30"
       style={{ backgroundColor: colors.background }}
     >
-      <div className="font-mono flex flex-col md:flex-row justify-center items-center text-center text-[#F5F5DC]">
+      <div className="font-mono flex flex-col md:flex-col justify-center items-center text-center text-[#F5F5DC]">
         <h1
-          className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl px-2"
+          className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl px-2"
           style={{
             textShadow: "3px 3px 0px #4A4A4A, 6px 6px 0px #2A2A2A",
           }}
         >
-          welcome
+          photography across the world
         </h1>
-        <p className="text-sm md:text-xl px-2">to some</p>
-        <h1
-          className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl px-2"
-          style={{
-            textShadow: "3px 3px 0px #4A4A4A, 6px 6px 0px #2A2A2A",
-          }}
-        >
-          photography
-        </h1>
-      </div>
-      <div
-        className="font-satisfy absolute left-6 top-1/3 -translate-y-2/3 text-[#F5F5DC] text-sm md:text-4xl w-20 md:w-40 "
-        style={{
-          textShadow: "2px 2px 0px #4A4A4A, 3px 3px 0px #2A2A2A",
-        }}
-      >
-        click a country to see more
+        <p className="text-sm md:text-lg px-2">click a country to see more</p>
       </div>
       {isModalOpen && currentImage && (
         <>
